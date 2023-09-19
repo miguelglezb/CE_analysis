017eb45356122e2949523c3def55868637292bb6
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-09-19 16:14:55 +1000

    Update scripts

--- scripts/check_data.sh
+++ scripts/check_data.sh
@@ -14,6 +14,7 @@ then
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=11O2F2MRESjQEGmkvlcxufhOc3WIHOzDk' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=11O2F2MRESjQEGmkvlcxufhOc3WIHOzDk" -O data/CE_example.zip && rm -rf /tmp/cookies.txt
     unzip data/CE_example.zip -d data/
     rm data/CE_example.zip
+    rm wget-log*
 else
     echo "\n $CE_dir seems to have the right dumpfiles\n"
 fi
