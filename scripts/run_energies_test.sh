413d2e57381e1990e017ca2b2a3db10fc2337175
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-09-20 20:16:42 +1000

    Fix bug with relative path

--- scripts/run_energies_test.sh
+++ scripts/run_energies_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "\n Run energy test... \n" 
-/bin/python3 /home/miguelgb/Workspaces/CE_analysis/src/energies.py
+/bin/python3 ./src/energies.py
--- scripts/run_sep_test.sh
+++ scripts/run_sep_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "\n Run separation test... \n" 
-/bin/python3 /home/miguelgb/Workspaces/CE_analysis/src/separation.py
+/bin/python3 ./src/separation.py
