00002734f4a5ada4b47d51245cbdaece925a86df
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-09-21 15:32:00 +1000

    Install exclusively texlive-extra-utils

--- scripts/install_font_dependencies.sh
+++ scripts/install_font_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#sudo apt install texlive-latex-extra texlive-fonts-recommended dvipng cm-super
-sudo apt install texlive-extra-utils texlive-fonts-extra cm-super
+#sudo apt install  texlive-fonts-recommended dvipng cm-super
+sudo apt install texlive-extra-utils #texlive-fonts-extra cm-super texlive-latex-extra
 
