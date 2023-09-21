a39647fffd830eb5cb8b875ded44520c542cd90f
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-09-21 15:46:39 +1000

    Take out sudo from uninstall_font_dependencies.sh

--- scripts/uninstall_font_dependencies.sh
+++ scripts/uninstall_font_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt remove texlive-latex-extra texlive-fonts-recommended dvipng cm-super
-sudo apt remove texlive-extra-utils texlive-fonts-extra
+apt remove texlive-latex-extra texlive-fonts-recommended dvipng cm-super
+apt remove texlive-extra-utils texlive-fonts-extra
 
