4c78e8716bf7aef1c25ee90e6ab76f85ba451b63
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-09-26 07:55:19 +1000

    Add installation of dvipng

--- scripts/install_font_dependencies.sh
+++ scripts/install_font_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Install latex fonts for Times New Roman in plot labels
-sudo apt install texlive-extra-utils cm-super texlive-latex-extra
+sudo apt install texlive-extra-utils cm-super texlive-latex-extra  g
 
