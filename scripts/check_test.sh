14f217411f44b67756e025c8bcbb4d3e55e2ce93
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-09-27 08:27:55 +1000

    Edit message for tests

--- scripts/check_test.sh
+++ scripts/check_test.sh
@@ -4,19 +4,19 @@ sep_file=figs/separation.pdf
 mech_ener_file=figs/mech_ener.pdf
 
 if [ -f "$sep_file" ]; then
-    echo '\n Orbital separation test passed! \n'
+    echo '\n Orbital separation plot found! \n'
 else
-    echo '\n Orbital separation test failed! \n'
+    echo '\n ERROR: Orbital separation plot not found! \n'
 fi
 
 if [ -f "$mech_ener_file" ]; then
-    echo '\n Mechanical energy test passed! \n'
+    echo '\n Mechanical energy plot found! \n'
 else
-    echo '\n Mechanical energy test failed! \n'
+    echo '\n ERROR: Mechanical energy plot not found! \n'
 fi
 
 if [ -f "$sep_file" ] && [ -f "$mech_ener_file" ]; then
-    echo '\n Test passed!!!' 
+    echo '\n TEST PASSED!!!\n' 
 fi
 
 cat extras/CE_logo.txt
