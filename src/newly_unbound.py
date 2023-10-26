b335c7401ac4fd4df78c0767220e81d81d64a71d
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-10-27 09:48:29 +1100

    Modify calculation of nbins

--- src/newly_unbound.py
+++ src/newly_unbound.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     
     fig, ax = plt.subplots()
     ax.plot(ph_data['time']*yr, np.log10(ph_data['sep. 1'])) 
-    nbins = min([int(len(dump_list)/2),500])
+    nbins = min([int(len(dump_list)),500])
     print(nbins)
     sns.histplot(x=time_newly_unb,y=logR_newly_unb, ax=ax,color='r',bins=nbins)
     ax.grid()
